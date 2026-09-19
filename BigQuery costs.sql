74eca0bc56ae92b9bf3637ac4df60ba50f902297
KonstantinOsenov <84586750+KonstantinOsenov@users.noreply.github.com>
2026-09-19 18:22:14 +0200

    Update BigQuery costs.sql

--- BigQuery costs.sql
+++ BigQuery costs.sql
@@ -14,7 +14,7 @@ select --*
   -- 1 tebibyte and above     -- $7.50 / 1 tebibyte, per 1 month / account -- ~6.54 EUR
   -- check the latest here - https://cloud.google.com/bigquery/pricing
   , round(total_bytes_billed * 0.00000000000090949 * 6.54, 6) as estimated_costs_eur
-from `cybercrime-gcp-e785`.`region-europe-west1`.INFORMATION_SCHEMA.JOBS
+from `<project_id>`.`region-europe-west1`.INFORMATION_SCHEMA.JOBS
 where 1=1
   and creation_time >= "2026-09-01"
   and creation_time <  "2026-09-19"
